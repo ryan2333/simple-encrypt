8acac16032f511c97b0ad2e1127d5c5a028372ee
yuanhai.zhao <yuanhai.zhao@bkjk.com>
2021-01-28 12:12:43 +0800

    first

---
+++ encrypt/simple.go
@@ -0,0 +1,238 @@
+package encrypt
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/des"
+	"encoding/base64"
+	"errors"
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+/*
+AES实现的方式:
+
+1.电码本模式（Electronic Codebook Book (ECB)）
+2.密码分组链接模式（Cipher Block Chaining (CBC)）
+3.计算器模式（Counter (CTR)）
+4.密码反馈模式（Cipher FeedBack (CFB)）
+5.输出反馈模式（Output FeedBack (OFB)）
+*/
+
+func MakeAesEncrypt(key, orig []byte) (crypt []byte, err error) {
+	var (
+		block     cipher.Block
+		blockMode cipher.BlockMode
+	)
+
+	switch len(key) {
+	case 16, 24, 32:
+		break
+	default:
+		err = errors.New(fmt.Sprintf("invalid key size %d, must 16,24,32", len(key)))
+		return
+	}
+
+	if block, err = aes.NewCipher(key); err != nil {
+		return
+	}
+
+	// 创建加密Mode， 此处使用CBC
+	blockMode = cipher.NewCBCEncrypter(block, key)
+
+	// 对byte数组进行长度填充
+	orig = PKCS7Padding(orig, block.BlockSize())
+	crypt = make([]byte, len(orig))
+	blockMode.CryptBlocks(crypt, orig)
+
+	return
+}
+
+func ParseAesEncrypt(key, crypt []byte) (orig []byte, err error) {
+	var (
+		block     cipher.Block
+		blockMode cipher.BlockMode
+	)
+
+	switch len(key) {
+	case 16, 24, 32:
+		break
+	default:
+		err = errors.New(fmt.Sprintf("invalid key size %d, must 16,24,32", len(key)))
+		return
+	}
+
+	if block, err = aes.NewCipher(key); err != nil {
+		return
+	}
+
+	if block, err = aes.NewCipher(key); err != nil {
+		return
+	}
+
+	// 创建解密mod
+	blockMode = cipher.NewCBCDecrypter(block, key)
+
+	// 解密
+	orig = make([]byte, len(crypt))
+	blockMode.CryptBlocks(orig, crypt)
+	orig = PKCS7UnPadding(orig)
+	return
+}
+
+func PKCS7Padding(orig []byte, blockSize int) []byte {
+	padding := blockSize - (len(orig) % blockSize)
+	return append(orig, bytes.Repeat([]byte{byte(padding)}, padding)...)
+}
+
+func PKCS7UnPadding(orig []byte) []byte {
+	padding := int(orig[len(orig)-1])
+	return orig[:len(orig)-padding]
+}
+
+func MakeDesEncrypt(key, orig []byte) (crypt []byte, err error) {
+	var (
+		block     cipher.Block
+		blockMode cipher.BlockMode
+	)
+
+	if len(key) != 8 {
+		err = errors.New(fmt.Sprintf("invalid key size %d, must 8", len(key)))
+		return
+	}
+
+	if block, err = des.NewCipher(key); err != nil {
+		return
+	}
+
+	blockMode = cipher.NewCBCEncrypter(block, key)
+
+	orig = PKCS7Padding(orig, block.BlockSize())
+	crypt = make([]byte, len(orig))
+	blockMode.CryptBlocks(crypt, orig)
+	return
+}
+
+func ParseDesEncrypt(key, crypt []byte) (orig []byte, err error) {
+	var (
+		block     cipher.Block
+		blockMode cipher.BlockMode
+	)
+
+	if len(key) != 8 {
+		err = errors.New(fmt.Sprintf("invalid key size %d, must 8", len(key)))
+		return
+	}
+
+	if block, err = des.NewCipher(key); err != nil {
+		return
+	}
+
+	blockMode = cipher.NewCBCDecrypter(block, key)
+	orig = make([]byte, len(crypt))
+	blockMode.CryptBlocks(orig, crypt)
+	orig = PKCS7UnPadding(orig)
+
+	return
+}
+
+func Make3DesEncrypt(key, orig []byte) (crypt []byte, err error) {
+	var (
+		block     cipher.Block
+		blockMode cipher.BlockMode
+	)
+	// 3des key必须是24位
+	if len(key) != 24 {
+		err = errors.New(fmt.Sprintf("invalid key size %d, must 24", len(key)))
+		return
+	}
+
+	if block, err = des.NewTripleDESCipher(key); err != nil {
+		return
+	}
+
+	blockMode = cipher.NewCBCEncrypter(block, key[:8])
+	orig = PKCS7Padding(orig, block.BlockSize())
+	crypt = make([]byte, len(orig))
+	blockMode.CryptBlocks(crypt, orig)
+
+	return
+}
+
+func Parse3DesEncrypt(key, crypt []byte) (orig []byte, err error) {
+	var (
+		block     cipher.Block
+		blockMode cipher.BlockMode
+	)
+
+	if len(key) != 24 {
+		err = errors.New(fmt.Sprintf("invalid key size %d, must 24", len(key)))
+		return
+	}
+
+	if block, err = des.NewTripleDESCipher(key); err != nil {
+		return
+	}
+
+	blockMode = cipher.NewCBCDecrypter(block, key[:8])
+	orig = make([]byte, len(crypt))
+	blockMode.CryptBlocks(orig, crypt)
+	orig = PKCS7UnPadding(orig)
+
+	return
+}
+
+func MakeMd5SaltEncrypt(orig []byte) (crypt []byte, err error) {
+	return bcrypt.GenerateFromPassword(orig, 0)
+}
+
+func MakeBase64StrWithAes(key, orig []byte) (base64Str string, err error) {
+	crypt, err := MakeAesEncrypt(key, orig)
+	if err != nil {
+		return
+	}
+	return base64.StdEncoding.EncodeToString(crypt), nil
+}
+
+func ParseBase64StrWithAes(key []byte, base64Str string) (orig []byte, err error) {
+	crypt, err := base64.StdEncoding.DecodeString(base64Str)
+	if err != nil {
+		return
+	}
+	return ParseAesEncrypt(key, crypt)
+}
+
+func MakeBase64StrWithDes(key, orig []byte) (base64Str string, err error) {
+	crypt, err := MakeDesEncrypt(key, orig)
+	if err != nil {
+		return
+	}
+	return base64.StdEncoding.EncodeToString(crypt), nil
+}
+
+func ParseBase64StrWithDes(key []byte, base64Str string) (orig []byte, err error) {
+	crypt, err := base64.StdEncoding.DecodeString(base64Str)
+	if err != nil {
+		return
+	}
+	return ParseDesEncrypt(key, crypt)
+}
+
+func MakeBase64StrWith3Des(key, orig []byte) (base64Str string, err error) {
+	crypt, err := Make3DesEncrypt(key, orig)
+	if err != nil {
+		return
+	}
+	return base64.StdEncoding.EncodeToString(crypt), nil
+}
+
+func ParseBase64StrWith3Des(key []byte, base64Str string) (orig []byte, err error) {
+	crypt, err := base64.StdEncoding.DecodeString(base64Str)
+	if err != nil {
+		return
+	}
+	return Parse3DesEncrypt(key, crypt)
+}
---
+++ encrypt/simple_test.go
@@ -0,0 +1,127 @@
+package encrypt
+
+import (
+	"testing"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestMakeAesEncrypt(t *testing.T) {
+	key := []byte("hhhhhhhhhhhhhhhh")
+	crypt, err := MakeAesEncrypt(key, []byte("pip123"))
+	if err != nil {
+		t.Errorf("encrypt failed, err: %v", err.Error())
+		return
+	}
+	t.Logf("encry: %s", string(crypt))
+
+	orig, err := ParseAesEncrypt(key, crypt)
+	if err != nil {
+		t.Errorf("parse failed, err: %s", err.Error())
+		return
+	}
+	t.Log("str: ", string(orig))
+}
+
+func TestMakeDesEncrypt(t *testing.T) {
+	key := []byte("hhhhhhhh")
+	str := "pip2333"
+	crypt, err := MakeDesEncrypt(key, []byte(str))
+	if err != nil {
+		t.Errorf("get des failed, err: %v", err.Error())
+		return
+	}
+	t.Log("crypt: ", string(crypt))
+
+	orig, err := ParseDesEncrypt(key, crypt)
+	if err != nil {
+		t.Errorf("parse failed, err: %v", err.Error())
+		return
+	}
+	t.Log("orig: ", string(orig))
+}
+
+func TestMake3DesEncrypt(t *testing.T) {
+	key := []byte("hhhhhhhhhhhhhhhhhhhhhhhh")
+	crypt, err := Make3DesEncrypt(key, []byte("pip123456778"))
+	if err != nil {
+		t.Errorf("encryp failed, err: %v", err.Error())
+		return
+	}
+	t.Log("crypt: ", string(crypt))
+
+	orig, err := Parse3DesEncrypt(key, crypt)
+	if err != nil {
+		t.Errorf("parse failed, err: %v", err.Error())
+		return
+	}
+	t.Log("orig: ", string(orig))
+}
+
+func TestMakeMd5SaltEncrypt(t *testing.T) {
+	crypt, err := MakeMd5SaltEncrypt([]byte("pip123123123123"))
+	if err != nil {
+		t.Errorf("encrypt failed, err: %v", err.Error())
+		return
+	}
+	t.Log("crypt: ", string(crypt))
+	if bcrypt.CompareHashAndPassword(crypt, []byte("pip123123123123")) == nil {
+		t.Log("success")
+	} else {
+		t.Error("failed")
+	}
+}
+
+func TestMakeBase64StrWithAes(t *testing.T) {
+	key := []byte("1234567890123456")
+	bt, err := MakeBase64StrWithAes(key, []byte("pip123"))
+	if err != nil {
+		t.Errorf("base crypt failed, err: %v", err.Error())
+		return
+	}
+	t.Log("base aes str: ", string(bt))
+
+	str, err := ParseBase64StrWithAes(key, string(bt))
+	if err != nil {
+		t.Errorf("base parse failed, err: %v", err.Error())
+		return
+	}
+
+	t.Log("base aes parse: ", string(str))
+}
+
+func TestMakeBase64StrWithDes(t *testing.T) {
+	key := []byte("12345678")
+	bt, err := MakeBase64StrWithDes(key, []byte("pip123"))
+	if err != nil {
+		t.Errorf("base crypt failed, err: %v", err.Error())
+		return
+	}
+	t.Log("base des str: ", string(bt))
+
+	str, err := ParseBase64StrWithDes(key, string(bt))
+	if err != nil {
+		t.Errorf("base parse failed, err: %v", err.Error())
+		return
+	}
+
+	t.Log("base des parse: ", string(str))
+}
+
+func TestMakeBase64StrWith3Des(t *testing.T) {
+	key := []byte("123456789012345612345678")
+	bt, err := MakeBase64StrWith3Des(key, []byte("pip123"))
+	if err != nil {
+		t.Errorf("base crypt failed, err: %v", err.Error())
+		return
+	}
+	t.Log("base 3des str: ", string(bt))
+
+	str, err := ParseBase64StrWith3Des(key, string(bt))
+	if err != nil {
+		t.Errorf("base parse failed, err: %v", err.Error())
+		return
+	}
+
+	t.Log("base 3des parse: ", string(str))
+}
